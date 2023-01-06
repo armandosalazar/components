5a01c88bd6368633d1c59c78ccdab01ab51ab776
Armando Salazar <armandosalazar351@gmail.com>
2023-01-05 23:35:37 -0600

    Add container 100% width

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -7,8 +7,18 @@ class HomePageTemp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        color: Colors.orange,
-        child: const Text('Luis'),
+        color: Colors.green,
+        padding: const EdgeInsets.all(20.0),
+        margin: const EdgeInsets.all(10.0),
+        height: 80,
+        width: MediaQuery.of(context).size.width,
+        alignment: Alignment.center,
+        child: const Text('Luis', style: TextStyle(
+          backgroundColor: Colors.lightGreen,
+          color: Colors.white,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),),
       ),
     );
   }
