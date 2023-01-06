2cd5990b5d66f86f5829e0b724a7d331213ae5bd
Armando Salazar <armandosalazar351@gmail.com>
2023-01-06 00:05:40 -0600

    Add items with for to ListView

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
       home: HomePageTemp(),
--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
-  const HomePageTemp({Key? key}) : super(key: key);
+  HomePageTemp({Key? key}) : super(key: key);
+
+  final options = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
 
   @override
   Widget build(BuildContext context) {
@@ -10,16 +12,22 @@ class HomePageTemp extends StatelessWidget {
         title: const Text('Componentes temp'),
       ),
       body: ListView(
-        children:  const <Widget>[
-          ListTile(
-            title: Text('Alertas'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('Alertas'),
-          ),
-        ],
+        children: _createItems(),
       ),
     );
+  }
+
+  List<Widget> _createItems() {
+    List<Widget> list = <Widget>[];
+    for (var option in options) {
+      var listTile = ListTile(title: Text(option));
+      // list.add(listTile);
+      // list.add(const Divider());
+      // Esto se puede hacer de otra forma con el operador de cascada:
+      list
+        ..add(listTile)
+        ..add(const Divider());
+    }
+    return list;
   }
 }
