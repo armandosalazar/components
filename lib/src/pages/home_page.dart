ee7b1c30b75ac1b9ac20010a24dd5e4d19d8ecae
Armando Salazar <armandosalazar351@gmail.com>
2023-01-08 01:50:52 -0600

    Add Furute Builder

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -17,26 +17,56 @@ class HomePage extends StatelessWidget {
   Widget _list() {
     // Llamando a menuProvider
     // Esto se disparara solamente una vez si no usamos hot restart
-    print(menuProvider.options);
-    return ListView(
-      children: _listItems(),
+    // print(menuProvider.options);
+    // Usaremos aquí el Future Builder
+    // Esta alternativa no es buena porque puede que tarde en cargar demasiado
+    // la data
+    // menuProvider.loadData().then((options) {
+    //   print('_lista()');
+    //   print(options);
+    // });
+    // return ListView(
+    //   children: _listItems(),
+    // );
+
+    // Usaremos aquí el Future Builder
+    return FutureBuilder(
+      future: menuProvider.loadData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        // print('builder');
+        // print(snapshot);
+        // print(snapshot.data);
+        return ListView(
+          children: _listItems(snapshot.data),
+        );
+      },
     );
   }
 
-  List<Widget> _listItems() {
-    return [
-      ListTile(
-        title: Text('Hola mundo'),
-      ),
-      Divider(),
-      ListTile(
-        title: Text('Hola mundo'),
-      ),
-      Divider(),
-      ListTile(
-        title: Text('Hola mundo'),
-      ),
-      Divider(),
-    ];
+  // Operador de null ?
+  List<Widget> _listItems(List<dynamic>? data) {
+    final List<Widget> options = [];
+
+    data?.forEach((element) {
+      options
+        ..add(ListTile(
+          title: Text(element['texto']),
+          leading: const Icon(
+            Icons.account_circle,
+            color: Colors.blue,
+          ),
+          trailing: const Icon(
+            Icons.keyboard_arrow_right,
+            color: Colors.blue,
+          ),
+          onTap: () {
+            print('Tap');
+          },
+        ))
+        ..add(const Divider());
+    });
+
+    return options;
   }
 }
--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -1,15 +1,24 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart' show rootBundle;
 
 class _MenuProvider {
   List<dynamic> options = [];
 
   _MenuProvider() {
-    loadData();
+    // loadData();
   }
 
-  void loadData() {
+  // Corregimos esto para utilizar un Future Builder
+  Future<List<dynamic>> loadData() async {
     // Aquí se regresa un future
-    rootBundle.loadString('data/menu_opts.json').then((data) => print(data));
+    final data = await rootBundle.loadString('data/menu_opts.json');
+    Map dataMap = json.decode(data);
+    // print(data);
+    // print(dataMap);
+    // print(dataMap['nombreApp']);
+    options = dataMap['rutas'];
+    return options;
   }
 }
 
