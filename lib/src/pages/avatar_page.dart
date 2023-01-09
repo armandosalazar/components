6b075207eacc9e0d85a6f59e4dfb3fd0b3817466
Armando Salazar <armandosalazar351@gmail.com>
2023-01-09 00:43:59 -0600

    Add Avatar to AppBar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,30 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatars'),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://static-cse.canva.com/blob/975731/1600w-z_r_KC1WlmU.jpg'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 5.0),
+            child: CircleAvatar(
+              child: Text('MN'),
+              backgroundColor: Colors.pinkAccent,
+              foregroundColor: Colors.white,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 5.0,
+            ),
+            child: CircleAvatar(
+              child: Text('AS'),
+            ),
+          ),
+        ],
       ),
     );
   }
