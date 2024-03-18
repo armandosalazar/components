1cf6e677eda72dc7532d0d1cd7f3d2cf67e7f441
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2024-03-17 20:29:22 -0600

    feat: add stateful widget

--- lib/src/pages/dashboard_page.dart
+++ lib/src/pages/dashboard_page.dart
@@ -8,8 +8,41 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  int _counter = 0;
+
+  _addCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Colors.transparent,
+          width: double.maxFinite,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '$_counter',
+                  style: const TextStyle(
+                      fontSize: 36, fontWeight: FontWeight.bold),
+                ),
+              ),
+              TextButton(
+                onPressed: () => _addCounter(),
+                child: const Text('+'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,6 +1,7 @@
 import 'package:components/src/pages/alert_page.dart';
 import 'package:components/src/pages/avatar_page.dart';
 import 'package:components/src/pages/card_page.dart';
+import 'package:components/src/pages/dashboard_page.dart';
 import 'package:components/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     '/': (BuildContext context) => const HomePage(),
     '/alert': (BuildContext context) => const AlertPage(),
     '/avatar': (BuildContext context) => const AvatarPage(),
-    '/card': (BuildContext context) => const CardPage()
+    '/card': (BuildContext context) => const CardPage(),
+    '/dashboard': (BuildContext context) => const DashboardPage(),
   };
 }
